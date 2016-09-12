4b85099e260305404108002e2c4ecb9e938e784c
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2016-09-12 13:27:37 +0530

    Update and Build

--- frontend.py
+++ frontend.py
@@ -24,7 +24,7 @@ def fixnewlines(message):
 
 def enable_links(message):
     links = parser.links(message)
-    url_identifier = ["www","http","bit.ly","com",".co.in"]
+    url_identifier = ["www","http","bit.ly",".com",".co.in"]
     for link in links:
         flag = 0
         for keyword in url_identifier :
@@ -32,7 +32,7 @@ def enable_links(message):
                 flag = 1
                 break
         if flag is 0 :
-            continue
+            break
         http_link = link
         if not link.startswith('http'):
             http_link = "http://{}".format(link)
